b6fca677a6e10cb2ba074c2c55fde7a14c5c0454
wbcatholic-ch <bong0219@gmail.com>
2026-07-01 16:18:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'catholic-way-V8-1-14-67-ANDROID-PLAY-GATE-LAYER';
-const ASSET_VERSION = 'V8-1-14-67-ANDROID-PLAY-GATE-LAYER';
+const CACHE_VERSION = 'catholic-way-V8-1-14-68-ANDROID-PLAY-GATE-BANNER-UP';
+const ASSET_VERSION = 'V8-1-14-68-ANDROID-PLAY-GATE-BANNER-UP';
 function withVersion(path) {
   return path + '?v=' + ASSET_VERSION;
 }
